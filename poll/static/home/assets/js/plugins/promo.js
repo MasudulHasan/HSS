0d8a9d281092f1bb83e474a35e74591827f950a4
MasudulHasan <masudulhasanmasud690@gmail.com>
2016-05-24 20:07:45 +0600

    Home page Design updated

---
+++ poll/static/home/assets/js/plugins/promo.js
@@ -0,0 +1,5 @@
+$(function () {
+    $(".fullscreen-static-image").backstretch(["/static/home/assets/img-temp/promo/laptop_small.jpg",
+                                                "/static/home/assets/img-temp/promo/blog_small.jpg"],
+        {duration: 5000, fade: 1500});
+});
